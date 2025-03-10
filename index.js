03f049d50eecd3742582bb4f93be2978ca28d36e
byot-multi-game <synnaxsynnax@gmail.com>
2025-03-10 12:09:32 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Embed PDF</title>
+</head>
+<body>
+    <h1>Embedded PDF: BYOT with supplementary.pdf</h1>
+    <embed src="BYOT with supplementary.pdf" width="100%" height="600px" type="application/pdf">
+</body>
+</html>
